68eef215e2e09227425101a6cc7c28d2c4661973
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-27 13:30:46 -0700

    Added favicon and colors to checkout page

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -41,18 +41,18 @@ const Summary = () => {
   };
 
   return (
-    <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
-      <h2 className="text-lg font-medium text-gray-900">Order Summary</h2>
+    <div className="mt-16 rounded-lg bg-background px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+      <h2 className="text-lg font-medium text-text">Order Summary</h2>
       <div className="mt-6 space-y-4">
-        <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-          <div className="text-base font-xs text-gray-600">Cart total</div>
+        <div className="flex items-center justify-between border-t border-accent pt-4">
+          <div className="text-base font-xs text-gray-800">Cart total</div>
           <Currency value={price} />
         </div>
       </div>
       <Button
         disabled={items.length === 0}
         onClick={onCheckout}
-        className="w-full mt-6"
+        className="w-full mt-6 bg-primary text-text"
       >
         Checkout
       </Button>
--- components/footer.tsx
+++ components/footer.tsx
@@ -5,6 +5,12 @@ const Footer = () => {
         <p className="text-center text-xs text-text">
           &copy; 2023 BuzzyBees. All rights reserved.
         </p>
+        <p className="text-center text-xs text-text px-6 pt-6">
+          Please note that while BuzzyBees is a functional eCommerce shop, it is
+          only intended to be used as a portfolio piece and a working example of
+          a code template. BuzzyBees is not a registered business and does not
+          sell any of the listed products.
+        </p>
       </div>
     </footer>
   );
